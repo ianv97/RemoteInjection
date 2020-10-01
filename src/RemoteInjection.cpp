37ceb3c72e41fd0b2c35588c7a654b98618d083b
Ian Vaernet <ianalexisnet@hotmail.com>
2020-10-01 13:22:42 -0300

    Methods refactory and .exe updated

--- src/RemoteInjection.cpp
+++ src/RemoteInjection.cpp
@@ -37,7 +37,7 @@ int main()
 {
     // Obtenemos el método a utilizar para inyectar la DLL
     int method;
-    printf("Select DLL Injection \n(1=CreateRemoteThread, 2=SetWindowsHooxEx, 3=CreateUserThread, 4=CreateThreadEx): ");
+    printf("Select DLL Injection \n(1=SetWindowsHooxEx, 2=CreateRemoteThread, 3=CreateUserThread, 4=CreateThreadEx): ");
     scanf_s("%d", &method);
 
     // Obtenemos el PATH a la DLL a inyectar
@@ -45,33 +45,9 @@ int main()
     printf("Path de la dll a inyectar: ");
     scanf_s("%s", dllPath, sizeof(dllPath));
 
-    // Obtenemos el handle al proceso objetivo
-    int processId;
-    printf("Id del proceso objetivo: ");
-    scanf_s("%d", &processId);
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-    if (hProcess == NULL || hProcess == INVALID_HANDLE_VALUE) {
-        printf("No se pudo abrir el proceso objetivo\n");
-        return -1;
-    }
-
-    // Asignamos memoria en el proceso objetivo
-    PVOID remoteMemory = VirtualAllocEx(hProcess, NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    printf("Remote memory: %p\n", remoteMemory);
-
-    // Escribimos el nombre de la dll que queremos que el proceso objetivo cargue
-    SIZE_T bytesWritten = 0;
-    WriteProcessMemory(hProcess, remoteMemory, dllPath, sizeof(dllPath), &bytesWritten);
-
-    // --------------------------------- CREATE REMOTE THREAD -----------------------------------------------------
-    if (method == 1) {
-        // Creamos un thread que invoca loadlibrary en el proceso destino
-        DWORD thread_id = 0;
-        CreateRemoteThread(hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryA, remoteMemory, 0, &thread_id);
-    }
 
     // ----------------------------------- SET WINDOWS HOOK EX ---------------------------------------------------------
-    if (method == 2) {
+    if (method == 1) {
         // Cargamos la dll
         HMODULE dll = LoadLibraryA((LPCSTR)dllPath);
         if (dll == NULL) {
@@ -88,46 +64,74 @@ int main()
         getchar();
         UnhookWindowsHookEx(handle);
     }
-
-    // --------------------------------- CREATE USER THREAD -----------------------------------------------------
-    if (method == 3) {
-       // Obtenemos un Handle a NTDLL.DLL que contiene la función CreateUserThread
-        HMODULE hNtDllModule = GetModuleHandleA("ntdll.dll");
-        if (hNtDllModule == NULL)
-        {
-            return NULL;
+    else {
+        // Obtenemos el handle al proceso objetivo
+        int processId;
+        printf("Id del proceso objetivo: ");
+        scanf_s("%d", &processId);
+        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+        if (hProcess == NULL || hProcess == INVALID_HANDLE_VALUE) {
+            printf("No se pudo abrir el proceso objetivo\n");
+            return -1;
         }
 
-        // Instanciamos la función
-        functypeRtlCreateUserThread funcRtlCreateUserThread = (functypeRtlCreateUserThread)GetProcAddress(hNtDllModule, "RtlCreateUserThread");
-        if (!funcRtlCreateUserThread)
-        {
-            return NULL;
+        // Asignamos memoria en el proceso objetivo
+        PVOID remoteMemory = VirtualAllocEx(hProcess, NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+        printf("Remote memory: %p\n", remoteMemory);
+
+        // Escribimos el nombre de la dll que queremos que el proceso objetivo cargue
+        SIZE_T bytesWritten = 0;
+        WriteProcessMemory(hProcess, remoteMemory, dllPath, sizeof(dllPath), &bytesWritten);
+
+
+        // --------------------------------- CREATE REMOTE THREAD -----------------------------------------------------
+        if (method == 2) {
+            // Creamos un thread que invoca loadlibrary en el proceso destino
+            DWORD thread_id = 0;
+            CreateRemoteThread(hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryA, remoteMemory, 0, &thread_id);
         }
-        // Creamos un thread que invoca LoadLibraryA en el proceso destino usando CreateUserThread
-        HANDLE thread_id = NULL;
-        funcRtlCreateUserThread(hProcess, NULL, 0, 0, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, remoteMemory, &thread_id, NULL);
+
+        // --------------------------------- CREATE USER THREAD -----------------------------------------------------
+        if (method == 3) {
+            // Obtenemos un Handle a NTDLL.DLL que contiene la función CreateUserThread
+            HMODULE hNtDllModule = GetModuleHandleA("ntdll.dll");
+            if (hNtDllModule == NULL)
+            {
+                return NULL;
+            }
+
+            // Instanciamos la función
+            functypeRtlCreateUserThread funcRtlCreateUserThread = (functypeRtlCreateUserThread)GetProcAddress(hNtDllModule, "RtlCreateUserThread");
+            if (!funcRtlCreateUserThread)
+            {
+                return NULL;
+            }
+            // Creamos un thread que invoca LoadLibraryA en el proceso destino usando CreateUserThread
+            HANDLE thread_id = NULL;
+            funcRtlCreateUserThread(hProcess, NULL, 0, 0, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, remoteMemory, &thread_id, NULL);
+        }
+
+        // --------------------------------- NT CREATE THREAD EX -----------------------------------------------------
+        if (method == 4) {
+            // Obtenemos un Handle a NTDLL.DLL que contiene la función CreateUserThread
+            HMODULE hNtDllModule = GetModuleHandleA("ntdll.dll");
+            if (hNtDllModule == NULL)
+            {
+                return NULL;
+            }
+
+            // Instanciamos la función CreateThreadEx
+            functypeNtCreateThreadEx funcNtCreateThreadEx = (functypeNtCreateThreadEx)GetProcAddress(hNtDllModule, "NtCreateThreadEx");
+            if (!funcNtCreateThreadEx)
+            {
+                printf("No se pudo encontrar la función NtCreateThreadEx");
+                return NULL;
+            }
+
+            HANDLE hThread = NULL;
+            funcNtCreateThreadEx(&hThread, GENERIC_ALL, NULL, hProcess, (LPTHREAD_START_ROUTINE)LoadLibraryA, remoteMemory, FALSE, NULL, NULL, NULL, NULL);
+        }
     }
-
-    // --------------------------------- NT CREATE THREAD EX -----------------------------------------------------
-    if (method == 4) {
-        // Obtenemos un Handle a NTDLL.DLL que contiene la función CreateUserThread
-        HMODULE hNtDllModule = GetModuleHandleA("ntdll.dll");
-        if (hNtDllModule == NULL)
-        {
-            return NULL;
-        }
-
-        // Instanciamos la función CreateThreadEx
-        functypeNtCreateThreadEx funcNtCreateThreadEx = (functypeNtCreateThreadEx)GetProcAddress(hNtDllModule, "NtCreateThreadEx");
-        if (!funcNtCreateThreadEx)
-        {
-            printf("No se pudo encontrar la función NtCreateThreadEx");
-            return NULL;
-        }
-
-        HANDLE hThread = NULL;
-        funcNtCreateThreadEx(&hThread, GENERIC_ALL, NULL, hProcess, (LPTHREAD_START_ROUTINE)LoadLibraryA, remoteMemory, FALSE, NULL, NULL, NULL, NULL);
-    }
+    
     
 }
